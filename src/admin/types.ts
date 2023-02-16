580abc1ad2ab3100d2510301ebeb3f0c126c3c2e
dream_boy_vamsi7 <112703562+vamsi6301465787@users.noreply.github.com>
2023-02-16 21:23:40 +0530

    Update types.ts

--- src/admin/types.ts
+++ src/admin/types.ts
@@ -4,16 +4,32 @@ import * as mongoDB from "mongodb"
 export const collections:{users?: mongoDB.Collection}={}
 
 export async function connectToDatabase() {
-    const client:mongoDB.MongoClient=new mongoDB.MongoClient(
-        "mongodb+srv://vamsi:<password>@cluster0.s0tyjqp.mongodb.net/?retryWrites=true&w=majority"
+    // const client:mongoDB.MongoClient=new mongoDB.MongoClient(
+    //     "mongodb+srv://vamsi:vamsi4()(L@cluster0.s0tyjqp.mongodb.net/?retryWrites=true&w=majority"
    
-    )
+    // )
 
-    await client.connect()
+    // await client.connect()
 
-    const db:mongoDB.Db=client.db("studentDetails")
+    // const db:mongoDB.Db=client.db("studentDetailssssss")
     
-    const users:mongoDB.Collection=db.collection("registration")
+    // const users:mongoDB.Collection=db.collection("registrationsssss")
 
-    collections.users=users
+    // collections.users=users
+
+   
+            const { MongoClient, ServerApiVersion } = require('mongodb');
+            const uri = "mongodb+srv://vamsi:vamsi4()(L@cluster0.s0tyjqp.mongodb.net/?retryWrites=true&w=majority";
+            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+            client.connect((err: any) => {
+            const users = client.db("test").collection("devices");
+            // perform actions on the collection object
+            collections.users=users
+           // client.close();
+            });
+
 }
+
+	
+
+
